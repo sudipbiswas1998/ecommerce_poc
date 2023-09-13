decedfdef814be389544201763e2f651130c81af
sudip2.biswas <sudip2.biswas@ril.com>
2023-09-13 18:18:00 +0530

    add security whitelisting

--- src/main/java/com/sudip/ecommerce_poc/controller/ProductController.java
+++ src/main/java/com/sudip/ecommerce_poc/controller/ProductController.java
@@ -5,16 +5,14 @@ import com.sudip.ecommerce_poc.util.BLUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @Slf4j
 @RequestMapping("/product")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
     @Autowired
     private ProductService productService;
